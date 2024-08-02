780269b8d68ce5b119e141e074fcdbb96a3eabd6
Shahrukhkhan-rabby <shahrukhkhan3418198@gmail.com>
2024-08-03 05:19:37 +0600

    end of the max

--- max.js
+++ max.js
@@ -8,7 +8,35 @@ const army = 600;
 // console.log('minister er pola is bigger');
 // }
 
-function findLargest()
+// function findLargest(first, second, third){
+//     if(first > second && first > third){
+//         return first;
+//     }
+//     else if(second > third){
+//         return second;
+//     } 
+//     else{
+//         return third;
+//     }
+// }
+// const largest = findLargest(1345, 641, 941);
+// console.log('Largest is', largest)
+
+
+function findSmallest(first, second, third){
+    if(first < second && first < third){
+        return first;
+    }
+    else if(second < third){
+        return second;
+    }
+    else{
+        return third
+    }
+}
+
+const smallest = findSmallest(345, 654, 844);
+console.log('smallest is', smallest)
 /*
 if(business > minister && business > army){
     console.log('Business is bigger');
@@ -22,4 +50,4 @@ else{
     */
 
 var max = Math.max(business, minister, army);
-console.log('largest is', max);
+// console.log('largest is', max);
