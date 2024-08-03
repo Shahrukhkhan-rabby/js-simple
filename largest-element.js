e9a537f7d3657e5c256696d1c2c7ba219abf8ca9
Shahrukhkhan-rabby <shahrukhkhan3418198@gmail.com>
2024-08-03 14:57:33 +0600

    largest

---
+++ largest-element.js
@@ -0,0 +1,32 @@
+// function largestElement(numbers){
+//     let largest = numbers[0];
+//     for(let i = 0; i < numbers.length; i++){
+//         const element = numbers[i];
+//         console.log(element);
+//         if(element > largest){
+//             largest = element;
+//         }
+        
+//     }
+//     return largest;
+// }
+// const ages = [12, 54, 2, 75, 32,];
+// const oldest = largestElement(ages);
+// const oldest2 = largestElement([-2, -6, -17,]);
+// console.log('oldest2', oldest2)
+
+// function findLowestElement(numbers){
+//     if(numbers.length ===0){
+//         return null;
+//     }
+//     let lowest = numbers[0];
+// for(let i= 1; i < numbers.length; i++){
+//     if(numbers[i] < lowest){
+//         lowest = numbers[i];
+//     }
+// }
+// return lowest;
+// }
+// const numbers = [44, 23, 34, 32, 54, 5, 78]
+// const lowestElement = findLowestElement(numbers);
+// console.log(lowestElement);
